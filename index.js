27f06691efbea25f1566e39d2bdc02995a86bb66
Felipe <alves.f.felipe02@gmail.com>
2021-10-17 18:57:39 -0300

    sexto

--- index.js
+++ index.js
@@ -14,16 +14,14 @@ let message = "";
 const Filme = require("./models/filme");
 
 app.get("/", (req, res) => {
-  setTimeout(() => {message = "";}, 1000);
-  res.render("index", {message});
+  res.render("index",);
 });
 
 app.get("/catalogo", async (req, res) => {
-  setTimeout(() => {message = "";}, 1000)
   const filmes = await Filme.findAll();
 
   res.render("catalogo", {
-    filmes, message
+    filmes,
   });
 });
 
@@ -42,15 +40,11 @@ app.post("/cadastro", async (req, res) => {
   const { nome_filme, genero_filme, imagem_filme, classificacao_filme, duracao_filme, ano_filme, diretor_filme } = req.body;
 
   if (!nome_filme) {
-    res.render("cadastro", {
-      message: "Nome é obrigatório",
-    });
+    res.render("cadastro",);
   }
 
   else if (!imagem_filme) {
-    res.render("cadastro", {
-      message: "Imagem é obrigatório",
-    });
+    res.render("cadastro",);
   }
 
   else {
@@ -67,33 +61,11 @@ app.post("/cadastro", async (req, res) => {
     } catch (err) {
       console.log(err);
 
-      res.render("cadastro", {
-        message: "Ocorreu um erro ao cadastrar o Filme!",
-      });
+      res.render("cadastro",);
     }
   }
-  message = "Filme/Série cadastrado(a)!"
   res.redirect("/")
-});/*
-
-
-  const filme = await Filme.create({
-    nome_filme,
-    genero_filme,
-    imagem_filme,
-    classificacao_filme,
-    duracao_filme,
-    ano_filme,
-    diretor_filme,
-  });
-
-  
-  res.render("cadastro", {
-    filme,
-  });
-  message = "Filme/Série cadastrado(a)!"
-  res.redirect("/");
-});*/
+});
 
 app.get("/editar/:id", async (req, res) => {
   const filme = await Filme.findByPk(req.params.id);
@@ -121,8 +93,6 @@ app.post("/editar/:id", async (req, res) => {
   res.render("editar", {
     filme: filmeEditado,
   });
-    message = "Filme/Série editado(a)!"
-  res.redirect("/catalogo");
 })
 
 app.get("/deletar/:id", async (req, res) => {
@@ -134,16 +104,13 @@ app.get("/deletar/:id", async (req, res) => {
 });
 
 app.post("/deletar/:id", async (req, res) => {
-  setTimeout(() => {message = "";}, 1000);
   const filme = await Filme.findByPk(req.params.id);
 
-  await filme.destroy();
-
-  res.render("deletar", {
-    message: `Deletado com sucesso!`,
+  const filmeDeletado = await filme.destroy();
+  res.render("index", {
+    filme: filmeDeletado,
   });
-  res.redirect("/catalogo");
-
+  
 });
 
 /*
