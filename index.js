3d3148881aa15b6f4ee84e16ef59ce529f295e98
Felipe <alves.f.felipe02@gmail.com>
2021-10-18 19:57:21 -0300

    6

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 let message = "";
 
